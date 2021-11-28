564683204fbd0cc5f0d197326539d3f230adb741
kms <aaaa727@khu.ac.kr>
2021-11-28 23:44:00 +0900

    zuul테스트하는데 잘 안 됨 확인 바람.

--- example/example/src/main/java/orderservice/example/ExampleApplication.java
+++ example/example/src/main/java/orderservice/example/ExampleApplication.java
@@ -1,4 +1,4 @@
-package test.eureka.eurekaclient;
+package orderservice.example;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -7,17 +7,18 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
-public class EurekaclientApplication {
+public class ExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaclientApplication.class, args);
+		SpringApplication.run(ExampleApplication.class, args);
 	}
 
 	@GetMapping("/info")
 	public String info(@Value("${server.port}") String port){
-		return "Order 서비스의 기본 동작 Port : {" + port + "}";
+		return "User 서비스의 기본 동작 Port : {" + port + "}";
 	}
+
 }
